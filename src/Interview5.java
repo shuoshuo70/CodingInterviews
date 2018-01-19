48875134e93293dde0d5a6d4a7c59da943e63fec
shuoshuo70 <724722392@qq.com>
2018-01-19 22:48:34 +0800

    5----print reverse list node

--- src/Interview5.java
+++ src/Interview5.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /**
  * Created by shuoshu on 2018/1/19.
  */
@@ -7,16 +9,32 @@ public class Interview5 {
         ListNode head = new ListNode(2);
         head.next = new ListNode(3);
         head.next.next = new ListNode(1);
-        solution.printReverseNode(head);
+        solution.printReverseNode1(head);
     }
 
-    public void printReverseNode (ListNode head) {
+    public void printReverseNode1 (ListNode head) {
+        if (head == null) {
+            return;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        while (head != null) {
+            stack.push(head.val);
+            head = head.next;
+        }
+
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+
+    public void printReverseNode2 (ListNode head) {
         if (head == null) {
             return;
         }
 
         ListNode next = head.next;
-        printReverseNode(next);
+        printReverseNode2(next);
         System.out.println(head.val);
     }
 }
