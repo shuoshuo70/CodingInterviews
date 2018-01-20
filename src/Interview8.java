852d54034f65b0c505f85645045c819618221fd5
shuoshuo70 <724722392@qq.com>
2018-01-20 15:16:47 +0800

    8----find min value in a rotate array

---
+++ src/Interview8.java
@@ -0,0 +1,27 @@
+/**
+ * Created by shuoshu on 2018/1/20.
+ */
+public class Interview8 {
+    public int minValue(int[] nums) {
+        if (nums.length == 0) {
+            return -1;
+        }
+
+        int l = 0, r = nums.length - 1;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[l] < nums[r]) {
+                return nums[l];
+            }
+
+            if (nums[l] <= nums[mid]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        return nums[l];
+    }
+}
