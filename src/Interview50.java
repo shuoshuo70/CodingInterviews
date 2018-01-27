d248bd2084e34835e903767b8bfc4dd74e1302b6
shuoshuo70 <724722392@qq.com>
2018-01-27 20:03:35 +0800

    50----common ancestor

---
+++ src/Interview50.java
@@ -0,0 +1,27 @@
+/**
+ * Created by shuoshu on 2018/1/27.
+ */
+public class Interview50 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == null || q == null) {
+            return null;
+        }
+
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+
+        if (left != null) {
+            return left;
+        }
+
+        return right;
+    }
+}
