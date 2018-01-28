74bc03de6b795faf24da1d2420d36083d4bade99
shuoshuo70 <724722392@qq.com>
2018-01-28 13:49:32 +0800

    62----serialize and deserialize a tree

---
+++ src/Interview62.java
@@ -0,0 +1,64 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by shuoshu on 2018/1/28.
+ */
+public class Interview62 {
+    public static void main(String[] args) {
+        Interview62 solution = new Interview62();
+        TreeNode root = new TreeNode(1);
+        root.left =new TreeNode(2);
+        root.right = new TreeNode(3);
+        String s = solution.serialize(root);
+        solution.deserialize(s);
+    }
+
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        if (root == null) {
+            return null;
+        }
+
+        serialize(root, sb);
+        return sb.toString();
+    }
+
+    private void serialize(TreeNode root, StringBuilder sb) {
+        if (root != null) {
+            sb.append(root.val + ",");
+            serialize(root.left, sb);
+            serialize(root.right, sb);
+        } else {
+            sb.append("#,");
+        }
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data == null || data.length() == 0) {
+            return null;
+        }
+        String[] strs = data.split(",");
+        Queue<String> q = new LinkedList<>();
+
+        for (String str : strs) {
+            q.offer(str);
+        }
+
+        return deserialize(q);
+    }
+
+    private TreeNode deserialize(Queue<String> q) {
+        if (q.isEmpty() || "#".equals(q.peek())) {
+            q.poll();
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.parseInt(q.poll()));
+        root.left = deserialize(q);
+        root.right = deserialize(q);
+
+        return root;
+    }
+}
